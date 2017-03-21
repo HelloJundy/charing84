235ee050e16616e2b57d83ec565e4ba0cef25f3d
hellojundy <hellojundy@163.com>
2017-03-21 10:12:56 +0800

    add wechat gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resource :wechat, only: [:show, :create]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
